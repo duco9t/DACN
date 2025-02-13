5ae21d6c34bb7e30d7d09f67d5488cad80d8ba1e
duco9t <dung5715@gmail.com>
2025-02-13 22:49:44 +0700

    Script Google Analytics

--- src/pages/users/homePage/index.jsx
+++ src/pages/users/homePage/index.jsx
@@ -49,6 +49,22 @@ const HomePage = () => {
     }
   }, [products, valueSearch]);
 
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=AW-16850404656";
+    document.head.appendChild(script1);
+  
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-16850404656');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
   const handleClickSearch = (item) => {
     navigator(`${ROUTERS.USER.DETAILS}/${item._id}`, {
       state: { productId: item._id }
